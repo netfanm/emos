da4bac4e9cc1a328f872379a57b8f81f5c0d4044
ming.fan <ming.fan@kaiqigu.com>
2017-10-17 16:27:56 +0800

    完成基本样式编写

--- asset/models.py
+++ asset/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class IDC(models.Model):
+    name = models.CharField(max_length=32, verbose_name="IDC名称")
+    address = models.CharField(max_length=32, verbose_name="地点")
+    count = models.IntegerField()
+    price = models.CharField(max_length=20, verbose_name="价格")
+
